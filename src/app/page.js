57679043f1b957c6166ff1541022395b695849e0
010binary <augustine32fg@gmail.com>
2024-09-23 17:12:45 +0100

    Added the footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,17 @@ import { Teams } from "./components/Teams";
 import CardGrid from "./components/Insight";
 import ExploreProperties from "./components/Explore";
 import GetStartedSection from "./components/Bridge";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Teams />
-      <CardGrid/>
-      <ExploreProperties/>
-      <GetStartedSection/>
-      
+      <CardGrid />
+      <ExploreProperties />
+      <GetStartedSection />
+      <Footer />
     </>
   );
 }
